efc662c716f43c57a3baff3b4ebe59dd1b81b58c
Mr. Blue <info@mrblue.io>
2024-02-18 23:08:29 +0100

    Cleanup

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,18 +1,10 @@
 // https://vitepress.dev/guide/custom-theme
-import { h } from 'vue'
-import Theme from 'vitepress/theme'
 import DefaultTheme from 'vitepress/theme'
 import './style.css'
 
 export default {
-  ...Theme,
-  Layout: () => {
-    return h(Theme.Layout, null, {
-      // https://vitepress.dev/guide/extending-default-theme#layout-slots
-    })
-  },
-  enhanceApp(ctx) {
-    DefaultTheme.enhanceApp(ctx)
+  extends: DefaultTheme,
+  enhanceApp({app}) {
     // ...
   }
 }
