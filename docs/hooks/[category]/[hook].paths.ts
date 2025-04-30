cf334698fd86e1ce45d83e926a28f151cf649a2d
Lorenzo <lturcotte@gmail.com>
2025-04-30 02:12:21 -0400

    HookDescription added HookDescription.replace("\r", "  ") to do a crlf not sure why the /r/n is already converted to just /r

--- docs/hooks/[category]/[hook].paths.ts
+++ docs/hooks/[category]/[hook].paths.ts
@@ -86,7 +86,7 @@ function getHookDescription(hooks: IHook[]) {
 
   for (const hook of hooks) {
     if (hook.HookDescription && !output.includes(hook.HookDescription)) {
-      output.push(hook.HookDescription);
+      output.push(hook.HookDescription.replace("\r", "  "));
     }
   }
 
